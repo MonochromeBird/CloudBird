b34f2cd12774d35b530120200358e3283fa54797
Alek Sean <devalek@protonmail.com>
2021-10-30 00:35:51 -0300

    Compare script working

--- tools/check.py
+++ tools/check.py
@@ -5,29 +5,29 @@ from os.path import exists, isfile
 from appdirs import user_data_dir
 
 tools_path   = '/'.join(__file__.split('/')[:-1:])
-programdir = user_data_dir('CloudBird')
+datadir = user_data_dir('CloudBird')
 try: mkdir(datadir)
 except: pass
 
 def generate(path: str) -> str:
-	return check_output(f'{tools_path}/sha256rec {path}', shell = True).decode()
+	return check_output(f'{tools_path}/sha256rec {path[:-1:] if path.endswith("/") else path}', shell = True).decode()
 
 def save(path: str = getcwd()) -> str:
 	sum = generate(path)
-	with open(datadir+f'currentsum.{path}', 'w') as sumfile:
+	with open(datadir+f'/currentsum', 'w') as sumfile:
 		sumfile.write(sum)
 	return sum
 
 def compare(path: str = getcwd()) -> bool:
-	if exists(datadir+f'currentsum.{path}'):
-		with open(datadir+f'currentsum.{path}') as sumfile:
+	if exists(datadir+f'/currentsum'):
+		with open(datadir+f'/currentsum') as sumfile:
 			previous_sum =  sumfile.read()
 	else: previous_sum = ""
 
 	sum = generate(path)
 
 	if sum != previous_sum:
-		with open(datadir+f'currentsum.{path}', 'w') as sumfile:
+		with open(datadir+f'/currentsum', 'w') as sumfile:
 			sumfile.write(sum)
 		return True
-	else: False
+	return False
