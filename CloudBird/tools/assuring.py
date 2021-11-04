59d8a0f751e1225d3409b1978d74fa62248db838
DevAlek <alek@monochromebird.xyz>
2021-11-04 17:20:55 -0300

    clear

--- CloudBird/tools/assuring.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from os import mkdir
-
-def assure_path(paths: list) -> None
-	for path in paths:
-		try: mkdir(path)
-		except: pass
--- setupfile.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from os import check_output, PIPE
-with open("cloudbirdinstall.log","a") as log:
-	log.write(check_output('sudo cp cloudbird.service /lib/systemd/system/', shell = True, stdout=PIPE).decode())
-	log.write(check_output('sudo cp * /usr/bin/ -rf', shell = True, stdout=PIPE).decode())
-print('Active `cloudbird.service` for a better experience.')
-exit(0)
