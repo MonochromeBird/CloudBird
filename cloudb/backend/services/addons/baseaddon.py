ac9b93f258fbb9d0b8b47d6602ea5b20c572a18e
DevAlek <DevAlek@protonmail.com>
2021-12-09 10:00:36 -0300

    [+] Added base addon

---
+++ cloudb/backend/services/addons/baseaddon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from ...utils import *
+from ... import app
+
+'''
+Base addon for CloudBird.
+'''
+
+def bake(self: object) -> None:
+	pass
+
+def upload(self: object) -> None: 
+	pass
+
+def download(self: object) -> None:
+	pass
